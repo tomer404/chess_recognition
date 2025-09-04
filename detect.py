a2ab92156c428fb40390718b5da66b14a228e721
Tomer Gisser <tomergisser11@gmail.com>
2025-09-04 18:36:22 +0300

    new version

--- detect.py
+++ detect.py
@@ -4,7 +4,8 @@ import os
 import numpy as np
 import math
 import torch
-model = YOLO(r"runs/detect/train/weights/best.pt")
+model = YOLO(r"runs/detect/train5/weights/best.pt")
+pieces_model = YOLO(r"runs2/detect/train/weights/best.pt")
 def create_file_path(img_num, folder_num):
     if img_num<10:
         str_i = "00"+str(img_num)
@@ -20,6 +21,10 @@ def create_file_path(img_num, folder_num):
         folder_str = str(folder_num)
     img_name = r"images/"+str(folder_num)+"/G"+folder_str+"_IMG"+str_i+".jpg"
     return img_name
+
+
+def create_folder_path(folder_num):
+    return r"images\\"+str(folder_num)
 
 
 def mid_point_of_boxes(coords):
@@ -55,6 +60,53 @@ def detect_corners_and_orientation(path_to_img):
     ones_coords = mid_point_of_boxes(ones_coords)
     eights_coords = mid_point_of_boxes(eights_coords)
     return [corners_coords, ones_coords, eights_coords]
+
+
+def recognize_pieces(img):
+    results = pieces_model.detect(source = img, conf = 0.1, iou = 0.5, agnostic_nm = False)
+    r = results[0]
+    boxes = r.boxes
+    class_ids = boxes.cls.cpu().numpy()
+    coords = boxes.coords.cpu().numpy()
+    coords_split_cls = [coords[class_ids == i] for i in range(12)]
+    return coords_split_cls
+
+
+def get_original_point(point, original_shape, corner_y, corner_x, cropped_shape):
+    '''get the coordinates of the point in the original image, before cropping and streching'''
+    h, w = original_shape[:2]
+    cropped_h, cropped_w = cropped_shape[:2]
+    size = max(cropped_h, cropped_w)
+    y, x = point
+    if(size == cropped_h):
+        x = int(cropped_w/size*x)
+    else:
+        y = int(cropped_h/size*y)
+    y, x = y + corner_y, x + corner_x
+    return x, y
+
+def get_point_in_board(point, shape, corners):
+    h, w = shape[:2]
+    src = np.float32([0, 0], [h, 0], [h, w], [w, 0])
+    (tl, tr, br, bl) = corners
+
+    widthA = np.linalg.norm(br - bl)
+    widthB = np.linalg.norm(tr - tl)
+    heightA = np.linalg.norm(tr - br)
+    heightB = np.linalg.norm(tl - bl)
+
+    maxW = int(math.ceil(max(widthA, widthB)))
+    maxH = int(math.ceil(max(heightA, heightB)))
+    maxW = max(maxW, 10)
+    maxH = max(maxH, 10)
+    dst = np.array([
+        [0, 0],
+        [maxW - 1, 0],
+        [maxW - 1, maxH - 1],
+        [0, maxH - 1]
+    ], dtype=np.float32)
+def get_square_of_piece(coords, img, corners, margin):
+    pass
 
 
 def distance(pt1, pt2):
@@ -142,7 +194,25 @@ def warp_quad(path_to_img, quad_pts, ones, eights, scale=1.0, margin_ratio=0, ma
     img_bgr = cv2.imread(path_to_img)
     M = cv2.getPerspectiveTransform(ordered, dst)
     warped = cv2.warpPerspective(img_bgr, M, (maxW, maxH))
-    return warped
+    h, w = warped.shape[:2]
+    size = max(h, w)
+    square_img = cv2.resize(warped, (size, size))
+    return square_img
+
+
+def crop_rectangle(path_to_img, quad_pts, margin = 0):
+    '''This method crops a bounding rectangle of the board'''
+    min_x = min(quad_pts[0][0], quad_pts[1][0], quad_pts[2][0], quad_pts[3][0]) - margin
+    max_x = max(quad_pts[0][0], quad_pts[1][0], quad_pts[2][0], quad_pts[3][0]) + margin
+    min_y = min(quad_pts[0][1], quad_pts[1][1], quad_pts[2][1], quad_pts[3][1]) - margin
+    max_y = max(quad_pts[0][1], quad_pts[1][1], quad_pts[2][1], quad_pts[3][1]) + margin
+    img = cv2.imread(path_to_img)
+    cropped_img = img[min_y:max_y, min_x:max_x]
+    h, w = cropped_img.shape[:2]
+    size = max(h, w)
+    square_img = cv2.resize(cropped_img, (size, size))
+    return square_img
+    
 
 def draw_results(path_to_img, corners, ones, eights):
     img = cv2.imread(path_to_img)
@@ -159,7 +229,7 @@ def draw_results(path_to_img, corners, ones, eights):
 def detect_and_crop(image_num, folder_num):
     path_to_img = create_file_path(image_num, folder_num)
     corners, ones, eights = detect_corners_and_orientation(path_to_img)
-    warped = warp_quad(path_to_img, corners, ones, eights, margin=150)
+    warped = warp_quad(path_to_img, corners, ones, eights, margin=210)
     cv2.namedWindow("Board", cv2.WINDOW_NORMAL)  # or: cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO
     cv2.imshow("Board", warped)
 
@@ -182,16 +252,34 @@ def draw_results(image_num, folder_num):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def save_cropped_files(folder_num, folder_name):
+def save_warped_files(folder_num, folder_name):
     os.makedirs(folder_name, exist_ok = True)
-    for i in range(126):
+    folder_path = create_folder_path(folder_num)
+    files = os.listdir(folder_path)
+    for i in range(len(files)):
         path_to_img = create_file_path(i, folder_num)
         corners, ones, eights = detect_corners_and_orientation(path_to_img)
         if len(corners)>=4:
-            warped = warp_quad(path_to_img, corners, ones, eights, margin=150)
+            warped = warp_quad(path_to_img, corners, ones, eights, margin=210)
             save_dir = os.path.join(folder_name, str(folder_num))
             os.makedirs(save_dir, exist_ok = True)
             saved_img_path = os.path.join(save_dir, f"{i}.png")
             cv2.imwrite(saved_img_path, warped)
             
-save_cropped_files(0, "warped imgs")
+
+def save_cropped_files(folder_num, folder_name):
+    os.makedirs(folder_name, exist_ok = True)
+    folder_path = create_folder_path(folder_num)
+    files = os.listdir(folder_path)
+    for i in range(len(files)):
+        path_to_img = create_file_path(i, folder_num)
+        corners, ones, eights = detect_corners_and_orientation(path_to_img)
+        if len(corners)>=4:
+            cropped = crop_rectangle(path_to_img, corners, margin = 120)
+            save_dir = os.path.join(folder_name, str(folder_num))
+            os.makedirs(save_dir, exist_ok = True)
+            saved_img_path = os.path.join(save_dir, f"{i}.png")
+            cv2.imwrite(saved_img_path, cropped)
+
+
+save_cropped_files(0, "cropped imgs")
